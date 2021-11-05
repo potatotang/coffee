4e836293bba7c52204a90e7649f91d800a363b4d
user <user@DESKTOP-1307>
2021-11-05 17:48:03 +0900

    day10 last commit

--- kingdom/src/day09/Ex03_01.java
+++ kingdom/src/day09/Ex03_01.java
@@ -4,9 +4,8 @@ import java.util.*;
 import javax.swing.*;
 
 
-@SuppressWarnings("rawtypes")
 public class Ex03_01 {
-	HashMap studMap = new HashMap();
+	HashMap<String, HashMap<String, Object>> studMap = new HashMap<String, HashMap<String, Object>>();
 
 	public Ex03_01() {
 		ArrayList<String> names = new ArrayList<String>();
@@ -23,10 +22,10 @@ public class Ex03_01 {
 		subNames.add("jsp");
 		subNames.add("spring");
 		
-		HashMap map = new HashMap();
-		ArrayList subj = null;
+		HashMap<String, ArrayList<Integer>> map = new HashMap<String, ArrayList<Integer>>();
+		ArrayList<Integer> subj = null;
 		for(int i = 0 ; i < subNames.size(); i++ ) {
-			subj = new ArrayList();
+			subj = new ArrayList<Integer>();
 			for(int j = 0 ; j < names.size(); j++ ) {
 				int score = (int)(Math.random()*41 + 60);
 				subj.add(score);
@@ -34,27 +33,27 @@ public class Ex03_01 {
 			map.put(subNames.get(i), subj);
 		}
 		
-		HashMap sMap = null;
+		HashMap<String, Object> sMap = null;
 		for(int i = 0 ; i < names.size(); i++ ) {
-			sMap = new HashMap();
+			sMap = new HashMap<String, Object>();
 			sMap.put("name", names.get(i));
 			for(int j = 0 ; j < subNames.size(); j++ ) {
-				sMap.put(subNames.get(j), ((ArrayList)map.get(subNames.get(j))).get(i));
+				sMap.put(subNames.get(j), map.get(subNames.get(j)).get(i));
 			}
 			studMap.put(names.get(i), sMap);
 		}
 		
 		// 총점 평균 계산
 		for(int i = 0 ; i < names.size(); i++ ) {
-			String name = (String)((HashMap)studMap.get(names.get(i))).get("name");
+			String name = (String)studMap.get(names.get(i)).get("name");
 			int t = 0 ;
 			for(int j = 0 ; j < subNames.size(); j++ ) {
-				int no = (int)((HashMap)studMap.get(name)).get(subNames.get(j));
+				int no = (int)studMap.get(name).get(subNames.get(j));
 				t += no;
 			}
 			
-			((HashMap)studMap.get(names.get(i))).put("total", t);
-			((HashMap)studMap.get(names.get(i))).put("avg", t / 5.0);
+			studMap.get(names.get(i)).put("total", t);
+			studMap.get(names.get(i)).put("avg", t / 5.0);
 		}
 		
 		// 출력
@@ -65,12 +64,12 @@ public class Ex03_01 {
 	
 	// 전체 결과 출력용 함수
 	public void toPrint() {
-		Set nameSet = studMap.keySet();
+		Set<String> nameSet = studMap.keySet();
 		Iterator<String> itor = nameSet.iterator();
 		while(itor.hasNext()) {
 			String student = itor.next();
-			HashMap resultMap = (HashMap)studMap.get(student);
-			Set subjKey = resultMap.keySet();
+			HashMap<String, Object> resultMap = studMap.get(student);
+			Set<String> subjKey = resultMap.keySet();
 			Iterator<String> subjIt = subjKey.iterator();
 			String subjName = subjIt.next();
 			StringBuffer buff = new StringBuffer();
@@ -91,8 +90,8 @@ public class Ex03_01 {
 	
 	// 한사람 데이터 출력용 함수
 	public void toPrint(String name) {
-		HashMap resultMap = (HashMap) studMap.get(name);
-		Set subjKey = resultMap.keySet();
+		HashMap<String, Object> resultMap = studMap.get(name);
+		Set<String> subjKey = resultMap.keySet();
 		StringBuffer buff = new StringBuffer();
 		Iterator<String> subjIt = subjKey.iterator();
 		while(subjIt.hasNext()) {
@@ -112,7 +111,7 @@ public class Ex03_01 {
 	
 	// 한 사람의 한과목 점수 조회
 	public void toPrint(String name, String subj) {
-		HashMap resultMap = (HashMap)studMap.get(name);
+		HashMap<String, Object> resultMap = studMap.get(name);
 		StringBuffer buff = new StringBuffer();
 		buff.append(String.format("%-7s : %5s", "name", name) + "\n");
 		buff.append(String.format("%-7s : " + ((subj.equals("avg")) ? "%7.2f": "%7d"), subj, resultMap.get(subj)));
