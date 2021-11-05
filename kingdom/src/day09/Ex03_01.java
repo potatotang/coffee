b453f7198c706dddffa12fce3387730247ad2105
user <user@DESKTOP-1307>
2021-11-05 09:46:34 +0900

    ex03_01 added...

--- kingdom/src/day09/Ex03_01.java
+++ kingdom/src/day09/Ex03_01.java
@@ -29,6 +29,45 @@ public class Ex03_01 {
 			map.put(subNames.get(i), subj);
 		}
 		
+		HashMap studMap = new HashMap();
+		HashMap sMap = null;
+		for(int i = 0 ; i < names.size(); i++ ) {
+			sMap = new HashMap();
+			sMap.put("name", names.get(i));
+			for(int j = 0 ; j < subNames.size(); j++ ) {
+				sMap.put(subNames.get(j), ((ArrayList)map.get(subNames.get(j))).get(i));
+			}
+			studMap.put(names.get(i), sMap);
+		}
+		
+		// 총점 평균 계산
+		for(int i = 0 ; i < names.size(); i++ ) {
+			String name = (String)((HashMap)studMap.get(names.get(i))).get("name");
+			int t = 0 ;
+			for(int j = 0 ; j < subNames.size(); j++ ) {
+				int no = (int)((HashMap)studMap.get(name)).get(subNames.get(j));
+				t += no;
+			}
+			
+			((HashMap)studMap.get(names.get(i))).put("total", t);
+			((HashMap)studMap.get(names.get(i))).put("avg", t / 5.0);
+		}
+		
+		// 출력
+		Set nameSet = studMap.keySet();
+		Iterator<String> itor = nameSet.iterator();
+		while(itor.hasNext()) {
+			String student = itor.next();
+			HashMap resultMap = (HashMap)studMap.get(student);
+			Set subjKey = resultMap.keySet();
+			Iterator<String> subjIt = subjKey.iterator();
+			while(subjIt.hasNext()) {
+				String subjName = subjIt.next();
+				System.out.println(subjName + " : " + resultMap.get(subjName));
+			}
+			System.out.println();
+		}
+		
 	}
 
 	public static void main(String[] args) {
