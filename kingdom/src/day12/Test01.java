cb9800feb15b2548e997b8a48e93bd566f741344
user <user@DESKTOP-1307>
2021-11-09 09:11:32 +0900

    day12 1st commit

---
+++ kingdom/src/day12/Test01.java
@@ -0,0 +1,26 @@
+package day12;
+
+/*
+	예츠의 오이진의 방랑기를
+	파일에 저장해보자.
+
+The Wanderings of Oisin and Other Poems
+- William Butler Yeats -
+
+We rode in sorrow, with strong hounds three,
+Bran, Sgeolan, and Lomair,
+On a morning misty and mild and fair.
+The mist-drops hung on the fragrant trees,
+And in the blossoms hung the bees.
+We rode in sadness above Lough Lean,
+For our best were dead on Gavra's green.
+ */
+public class Test01 {
+
+	public Test01() {
+	}
+	
+	public static void main(String[] args) {
+		new Test01();
+	}
+}
